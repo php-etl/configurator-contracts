0a101820a844e9dc045c0a5cc8038715f0e06973
Grégory Planchat <gregory@kiboko.fr>
2020-12-09 08:02:30 +0100

    Initial commit with the configurator interface

---
+++ src/ServiceInterface.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Kiboko\Component\ETL\Contract\Configurator;
+
+use PhpParser\Node;
+
+interface ServiceInterface
+{
+    public function validate(array $config): bool;
+    /** @return array<Node> */
+    public function compile(array $config): array;
+    public function build(array $config): object;
+}
