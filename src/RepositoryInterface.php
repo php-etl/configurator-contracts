5f515c7a7dd1a7c91440b81eaaa1b537718f41b7
Grégory Planchat <gregory@kiboko.fr>
2021-01-27 21:42:48 +0100

    Added accessors to the RepositoryInterface

--- src/RepositoryInterface.php
+++ src/RepositoryInterface.php
@@ -7,7 +7,11 @@ use PhpParser\Builder;
 interface RepositoryInterface
 {
     public function addFiles(FileInterface ...$files): self;
+    /** @return iterable<FileInterface> */
+    public function getFiles(): iterable;
     public function addPackages(string ...$packages): self;
+    /** @return iterable<string> */
+    public function getPackages(): iterable;
     public function getBuilder(): Builder;
     public function merge(RepositoryInterface $friend): self;
 }
