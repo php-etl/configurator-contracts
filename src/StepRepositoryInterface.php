fc49ecbdefb694e126b037599663e09be476e063
Grégory Planchat <gregory@kiboko.fr>
2021-04-30 08:18:33 +0200

    Added the StepRepositoryInterface to add loggers to steps

---
+++ src/StepRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Kiboko\Contract\Configurator;
+
+use PhpParser\Node;
+
+interface StepRepositoryInterface extends RepositoryInterface
+{
+    public function withLogger(Node\Expr $logger);
+}
