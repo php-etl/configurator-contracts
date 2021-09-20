0afec8aa360c0ab946b5403687973e85fd1dac5e
Grégory Planchat <gregory@kiboko.fr>
2021-09-20 21:48:09 +0200

    Added PHP attributes: Adapter, Feature, Plugin and Runtime

---
+++ src/Feature.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Kiboko\Contract\Configurator;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class Feature
+{
+    public function __construct(
+        public string $name,
+    ) {}
+}
